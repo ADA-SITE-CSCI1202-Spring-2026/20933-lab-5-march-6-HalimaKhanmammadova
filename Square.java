7f7fbaa69e3ae68e2ce43f154736d62365625c49
halima khanmammadova <hkhanmammadova13941@ada.edu.az>
2026-03-06 13:52:48 +0400

    Update Square.java with equals and toString

--- Square.java
+++ Square.java
@@ -2,4 +2,18 @@ class Square extends Rectangle {
     public Square(int side) {
         super(side, side);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Square) {
+            Square s = (Square) obj;
+            return this.width == s.width;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Square(side=" + width + ")";
+    }
 }
