7c9358587d1b1927e13b0cc97a949005e62f8b45
halima khanmammadova <hkhanmammadova13941@ada.edu.az>
2026-03-06 13:10:37 +0400

    Exercise 2 ( all) rectangle

---
+++ Rectangle.java
@@ -0,0 +1,25 @@
+class Rectangle {
+    int width, height;
+
+    public Rectangle(int w, int h) {
+        width = w;
+        height = h;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Rectangle rect = (Rectangle) obj;
+        return this.width == rect.width && this.height == rect.height;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle(5, 10);
+        Rectangle r2 = new Rectangle(15, 10);
+        Rectangle r3 = new Rectangle(5, 10);
+
+        System.out.println(r1.equals(r2)); // false
+        System.out.println(r1.equals(r3)); // true
+    }
+}
